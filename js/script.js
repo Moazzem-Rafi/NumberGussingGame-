c048426a3ac473d0dd880bef5f0ced71f3931dfa
Moazzem-Rafi <mhr8june@gmail.com>
2026-05-24 00:03:59 +0600

    Secound & Third Page of JavaScript Done

--- js/script.js
+++ js/script.js
@@ -24,5 +24,41 @@ firstBtn.addEventListener("click", () => {
 // First Page End
 
 // Secound Page Start
+let secoundInput = document.getElementById("secoundinput");
+let secoundBtn = document.getElementById("secoundBtn");
+let secoundWarningText = document.getElementById("secoundWarningText");
+let thirdHidden = document.getElementById("third");
 
+secoundBtn.addEventListener("click", () => {
+  let inputValue = secoundInput.value;
+  if (secoundInput.value < 1 || secoundInput.value > 10) {
+    secoundWarningText.removeAttribute("hidden");
+    secoundInput.value = "";
+  } else {
+    secoundWarningText.setAttribute("hidden", true);
+    secoundHidden.setAttribute("hidden", true);
+    thirdHidden.removeAttribute("hidden");
+  }
+});
 // Secound Page End
+
+// Third Page Start
+let thirdHeading = document.getElementById("thirdHeading");
+let thirdInput = document.getElementById("thirdInput");
+let thirdBtn = document.getElementById("thirdBtn");
+let thirdWarningText = document.getElementById("thirdWarningText");
+let fourthHidden = document.getElementById("fourth");
+
+thirdBtn.addEventListener("click", () =>{
+let inputValue = thirdInput.value ;
+if (inputValue == "") {
+    thirdWarningText.removeAttribute("hidden");
+    thirdInput.value = "";
+} else {
+    thirdWarningText.setAttribute("hidden",true);
+    thirdInput.value = "";
+    thirdHidden.setAttribute("hidden" , true);
+    fourthHidden.removeAttribute("hidden");
+}
+});
+// Third Page End
